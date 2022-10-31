ff70d5cb0f5f25ad78122147a3c5ff5f3f6c82f4
omarkhaled646 <63152184+omarkhaled646@users.noreply.github.com>
2022-10-31 12:33:23 +0200

    updated with notebook of visualization & data preprocessing

--- DataManager.py
+++ DataManager.py
@@ -1,2 +1,31 @@
-
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 # TODO : add data reading, preprocessing
+
+encoder = LabelEncoder()
+
+def read(data):
+    penguins = pd.read_csv(data)
+    return penguins
+
+
+def preprocessing(data,dataFeatures , test = False):
+    for feature in dataFeatures:
+        if data[feature].dtypes == object:
+            data[feature].fillna(data[feature].mode()[0])
+        else:
+            data[feature].fillna(data[feature].mean())
+
+
+   
+    for feature in dataFeatures:
+        if data[feature].dtypes == object:
+            encoder.fit(data[feature])
+            data[feature] = encoder.transform(data[feature])
+
+   
+
+                  
+
+   
+
