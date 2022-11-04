de8e68680093b97e75a7eb4d6130e288bc8bbc9d
Bassel Ahmed <basselahmed95@gmail.com>
2022-11-04 17:47:02 +0200

    temporarily fit with hardcoded parameters in gui_logic.py

--- gui_logic.py
+++ gui_logic.py
@@ -3,10 +3,11 @@ from models.NeuralNetworks import Perceptron
 
 
 def train_button():
-    prep_data()
-    model = Perceptron()
-    model.fit()
-    # TODO: enable retrain and test buttons
+    data = prep_data()
+    model = Perceptron(learning_rate=0.1, epochs=10)
+    Y = data['body_mass_g']
+    X = data.drop('body_mass_g', axis=1)
+    model.fit(X, Y)
 
 def retrain_button():
     pass
