ff41abda482072ec1552a3a2f6c12fbb429543cf
omarkhaled646 <63152184+omarkhaled646@users.noreply.github.com>
2022-11-04 18:30:46 +0200

    updated with split & encode target functions

--- DataManager.py
+++ DataManager.py
@@ -10,7 +10,7 @@ filepath = "data/penguins.csv"
 
 def prep_data():
     data = read()
-    data = preprocessing(data, data.columns)
+    data = preprocessing(data, data.columns.drop("species"))
     return data
 
 
@@ -34,3 +34,30 @@ def preprocessing(data, dataFeatures, test=False):
     return data
 
 
+def encodeTargets(data,classes):
+    notWantedClass = [c for c in set(data["species"]) if c not in classes]
+    data["species"].replace(classes,[-1,1],inplace = True)
+    data = data[data["species"]!= notWantedClass[0]]
+
+    return data
+
+
+def split(data,features):
+       featuresToDrop = [feature for feature in  data.columns.drop("species") if feature not in features]
+       data.drop(featuresToDrop,inplace = True , axis = 1)
+       
+       trainData_for_class_0 = data[data["species"] == -1][:30]
+       trainData_for_class_1 = data[data["species"] == 1][:30] 
+       trainData = trainData_for_class_0.append(trainData_for_class_1)
+       
+       X_train = trainData.drop("species",axis = 1)
+       Y_train = trainData["species"]
+
+       testData_for_class_0 = data[data["species"] == -1][30:]
+       testData_for_class_1 = data[data["species"] == 1][30:] 
+       testData = testData_for_class_0.append(testData_for_class_1)
+       
+       X_test = testData.drop("species",axis = 1)
+       Y_test = testData["species"]
+
+       return X_train,X_test,Y_train,Y_test
