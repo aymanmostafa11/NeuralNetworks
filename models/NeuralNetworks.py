5e937ee0d8c0f56f7dd152340ab2247f90f1db4b
said-2001 <sadevip2001@gmail.com>
2022-11-30 01:29:47 +0200

    added back propagation

--- models/NeuralNetworks.py
+++ models/NeuralNetworks.py
@@ -142,6 +142,7 @@ class MLP(Model):
     def __init__(self, lr, layers: dict, bias=True):
         super().__init__(lr, bias)
         self._layers = layers
+        self._lr = lr
         """
             layers
             [
@@ -157,7 +158,7 @@ class MLP(Model):
         """
 
     def __init_weights(self, x: pd.DataFrame):
-        features_count = x.shape[1]
+        features_count = x.shape[0]
 
         """
             a list of matrices where each matrix represents the weights of a particular layer,
@@ -170,26 +171,39 @@ class MLP(Model):
 
         # Set random weights for each layer in the network
         for i in range(1, len(self._layers)):
-            self._weights.append(np.random.rand(self._layers[i - 1]['units'], self._layers[i]['units']))
+            self._weights.append(np.random.rand(self._layers[i]['units'], self._layers[i - 1]['units']))
 
-        if self._bias:
             """
                 a list of lists where each list represents the biases of a particular layer,
                 each list element represents the bias of a unit.
             """
             self._biases = []
             # Input layer "biases"
-            self._biases.append(np.zeros(self._layers[0]['units']))
+            self._biases.append(np.zeros((self._layers[0]['units'], 1)))
 
             # Set random biases for each layer in the network
             for i in range(1, len(self._layers)):
-                self._biases.append(np.random.rand(self._layers[i]['units']))
+                self._biases.append(np.zeros((self._layers[i]['units'], 1)))
 
     # Sa3ood back propagation
     def fit(self, x: pd.DataFrame | np.ndarray, y: pd.DataFrame | np.ndarray, epochs=100, verbose=True):
-        pass
+        """
+
+        Args:
+            x: Input -- should be (nx,m)
+            y: expected output -- should be (nL,1)
+            epochs: number of iterations
+        """
+
+        self.__init_weights(x)
+
+        for i in range(0, epochs):
+            AL, caches = self._forward(x)
+            gradients = self._backward(AL, y, caches)
+            self.update_weights(gradients, self._lr)
 
     # Forward Propagation
+
     def predict(self, x: pd.DataFrame | np.ndarray):
         return self._forward(x)
 
@@ -201,15 +215,105 @@ class MLP(Model):
             :param b: layer biases list.
             :param g: layer activation function.
         """
-        z = a_in @ W + b
+        z = np.dot(W, a_in) + b
         a_out = g(z)
-        return a_out
+        return a_out, z
 
     def _forward(self, x: np.ndarray):
         """
         :param x: sample input
         :return: prediction
         """
-        for i in range(len(self._layers)):
-            x = self._march(x, self._weights[i], self._biases[i], self._layers[i]['activation'])
-        return x
+        cache = {}
+        for l in range(len(self._layers)):
+            x, z = self._march(x, self._weights[l], self._biases[l], self._layers[l]['activation'])
+            cache["A" + str(l)] = x
+            cache["Z" + str(l)] = z
+
+        return x, cache
+
+    # Backward Propagation
+
+    def _retreat(self, dZ: np.ndarray, cache: dict, layer_num: int):
+
+        """
+        calculate gradiant for single layer
+        Args:
+            dZ: Gradient of the cost with respect to Z
+            cache: dictionary where we store A , Z for computing backward
+
+        Returns:
+            dW: Gradient of the cost with respect to W
+            dB: Gradient of the cost with respect to b
+            dZ: Gradient of the cost with respect to Z for previous layer
+        """
+        A = cache["A" + str(layer_num - 1)]
+        W = self._weights[layer_num]
+        Z = cache["Z" + str(layer_num - 1)]
+        m = A.shape[1]
+
+        # calculate gradients for the layer
+        dW = 1 / m * np.dot(dZ, A.T)
+        db = 1 / m * np.sum(dZ, axis=1, keepdims=True)
+        dA = np.dot(W.T, dZ)
+
+        if self._layers[layer_num]["activation"] == Activations.tanh:
+            dZ = tanh_backward(dA, Z)
+
+        elif self._layers[layer_num]["activation"] == Activations.sigmoid:
+            dZ = sigmoid_backward(dA, Z)
+        return dW, db, dZ
+
+    def _backward(self, AL: np.ndarray, Y: np.ndarray, caches: dict):
+
+        """
+        calculate gradiant for all layers
+
+        Args:
+            AL: predictions
+            Y: output
+            caches: dictionary where we store A , Z for computing backward
+
+        Returns: dictionary where we store Gradient of the cost with respect to W,b for all examples
+
+        """
+
+        gradients = {}
+        L = len(self._layers)  # the number of layers
+        m = Y.shape[1]
+        dZL = AL - Y  # compute first dZ
+        dZ = dZL
+        for layer_num in reversed(range(1, L)):
+            dW, db, dZ = self._retreat(dZ, caches, layer_num)
+            gradients["dW" + str(layer_num)] = dW
+            gradients["db" + str(layer_num)] = db
+        return gradients
+
+    def update_weights(self, gradients : dict, learning_rate : float):
+        """
+        Update parameters using gradient descent
+        Arguments:
+        gradients: dictionary containing your gradients
+        """
+        L = len(self._weights)  # number of layers in the neural network
+        for layer in range(1, L):
+            self._weights[layer] = self._weights[layer] - learning_rate * gradients['dW' + str(layer)]
+            self._biases[layer] = self._biases[layer] - learning_rate * gradients['db' + str(layer)]
+
+    def _calculate_cost(self):
+        pass
+
+    def _calculate_updates(self):
+        pass
+
+
+dic = {0: {"units": 3, "activation": Activations.linear}, 1: {"units": 5, "activation": Activations.tanh},
+       2: {"units": 4, "activation": Activations.tanh}, 3: {"units": 4, "activation": Activations.tanh},
+       4: {"units": 4, "activation": Activations.tanh}, 5: {"units": 3, "activation": Activations.tanh}}
+x = [[12, 54, 65], [121, 56, 46], [561, 25, 6], [11, 5, 36], [11, 15, 61], [21, 52, 26], [1, 5, 6]]
+y = [[1, 0, 0], [1, 0, 0], [0, 0, 1], [1, 0, 0], [1, 0, 0], [0, 0, 1], [1, 0, 0]]
+li1 = np.array(x).T
+li2 = np.array(y).T
+
+mod = MLP(.01, dic)
+mod.fit(li1, li2)
